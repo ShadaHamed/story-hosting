c5a1c5391d45f813c8b3e8cb44f67ec97fa98d89
unknown <shadahamed@aucegypt.edu>
2025-08-06 10:31:41 +0300

    edit domain2

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const STORY_PER_PAGE = 6;
 
-const PRODUCTION_DOMAIN = "https://cloud-hosting.vercel.app";
+const PRODUCTION_DOMAIN = "https://story-hosting.vercel.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
 export const DOMAIN = process.env.NODE_ENV === 'production' 
