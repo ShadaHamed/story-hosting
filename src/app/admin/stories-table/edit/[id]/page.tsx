76789dc212a3f9b40cfdb740d3c91e4e4502bf74
unknown <shadahamed@aucegypt.edu>
2025-08-06 15:17:34 +0300

    fix

--- src/app/admin/stories-table/edit/[id]/page.tsx
+++ src/app/admin/stories-table/edit/[id]/page.tsx
@@ -15,7 +15,7 @@ const EditArticlePage = async ({ params } : EditStoryPageProps) => {
     <section className='fix-height flex items-center justify-center px-5 lg:px-20'>
       <div className='shadow p-4 bg-purple-200 rounded w-full'>
         <h2 className='text-2xl text-green-700 font-semibold mb-4'>
-            Edit Article
+            Edit Story
         </h2>
         <EditStoryForm story={story} />
       </div>
